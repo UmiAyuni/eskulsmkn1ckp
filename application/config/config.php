0a3254d1ac2f23da9b1ea99025d35cfad2eb32c1
Umi Ayuni <137033806+UmiAyuni@users.noreply.github.com>
2023-07-06 08:33:07 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://umiayuni.github.io/eskulsmkn1ckp/';
+$config['base_url'] = 'http://localhost:/eskulsmkn1ckp/';
 
 /*
 |--------------------------------------------------------------------------
